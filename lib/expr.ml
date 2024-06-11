3d0f5e6e879aafc2e56a2c180eaabd4c8c85164f
Christian Rinderknecht <Christian.Rinderknecht@tezcore.com>
2024-06-11 14:15:30 +0200

    Change of names. Added a record (rule of thumb: triples should be records).

--- lib/expr.ml
+++ lib/expr.ml
@@ -25,7 +25,7 @@ module Base = struct
     | Bls12_381_fr of string
     | Signature of string
 
-  type 'expr contract = { parameter : type_; storage : type_; body : 'expr }
+  type 'expr contract = { parameter : type_; storage : type_; code : 'expr }
   [@@deriving map]
 
   type micheline = (location, string) Tezos_micheline.Micheline.node list
@@ -33,54 +33,58 @@ module Base = struct
   let map_row = Rose_tree.map
 
   type 'expr t =
-    (* basic lambda calculus w/ primivatives + constants *)
+    (* basic lambda calculus w/ primitives + constants *)
     | Var of var
-    | Let_in of { binding : var; let_ : 'expr; in_ : 'expr }
-    | Lambda of { binding : var; ty : type_; body : 'expr }
-    | Lambda_rec of { binding : var; recurse : var; ty : type_; body : 'expr }
-    | App of { func : 'expr; arg : 'expr }
+    | Let_in of { let_var : var; rhs : 'expr; in_ : 'expr }
+    | Lambda of { lam_var : var; return : type_; body : 'expr }
+    | Lambda_rec of {
+        lam_var : var
+      ; mu_var : var
+      ; return : type_
+      ; body : 'expr }
+    | App of { abs : 'expr; arg : 'expr }
     | Const of const
     | Prim0 of type_ Primitive.t0
     | Prim1 of type_ Primitive.t1 * 'expr
     | Prim2 of type_ Primitive.t2 * 'expr * 'expr
     | Prim3 of type_ Primitive.t3 * 'expr * 'expr * 'expr
     (* mutability *)
-    | Let_mut_in of { binding : mut_var; let_ : 'expr; in_ : 'expr }
+    | Let_mut_in of { let_var : mut_var; rhs : 'expr; in_ : 'expr }
     | Deref of mut_var
     | Assign of mut_var * 'expr
     (* low-level control flow (conditional) *)
-    | If_bool of { condition : 'expr; on_true : 'expr; on_false : 'expr }
-    | If_none of { scrutinee : 'expr; on_none : 'expr; on_some : var * 'expr }
+    | If_bool of { condition : 'expr; if_true : 'expr; if_false : 'expr }
+    | If_none of { subject : 'expr; if_none : 'expr; if_some : var * 'expr }
     | If_cons of {
-          scrutinee : 'expr
-        ; on_empty : 'expr
-        ; on_nonempty : var * var * 'expr
+          subject : 'expr
+        ; if_empty : 'expr
+        ; if_nonempty : var * var * 'expr
       }
     | If_left of {
-          scrutinee : 'expr
-        ; on_left : var * 'expr
-        ; on_right : var * 'expr
+          subject : 'expr
+        ; if_left : var * 'expr
+        ; if_right : var * 'expr
       }
     (* low-level control flow (iterative) *)
-    | While of { condition : 'expr; body : 'expr }
-    | While_left of { condition : 'expr; body : 'expr }
+    | While of { invariant : 'expr; body : 'expr }
+    | While_left of { invariant : 'expr; body : 'expr }
     | For of {
-          counter : mut_var
+          index : mut_var
         ; init : 'expr
-        ; condition : 'expr
-        ; update : 'expr
+        ; invariant : 'expr
+        ; variant : 'expr
         ; body : 'expr
       }
-    | For_each of var list * 'expr * 'expr
+    | For_each of {indices : var list; collection : 'expr; body : 'expr}
     (* high-level control flow (iterative) *)
-    | Map of { container : 'expr; function_ : var list * 'expr }
-    | Fold_left of { container : 'expr; init : 'expr; fold : var * 'expr }
-    | Fold_right of { container : 'expr; init : 'expr; fold : var * 'expr }
+    | Map of { collection : 'expr; map : var list * 'expr }
+    | Fold_left of { collection : 'expr; init : 'expr; fold : var * 'expr }
+    | Fold_right of { collection : 'expr; init : 'expr; fold : var * 'expr }
     (* tuples *)
-    | Let_tuple_in of { bindings : var list; let_ : 'expr; in_ : 'expr }
+    | Let_tuple_in of { components : var list; rhs : 'expr; in_ : 'expr }
     | Tuple of 'expr row
     | Proj of 'expr * row_path
-    | Update of { tuple : 'expr; element : row_path; update : 'expr }
+    | Update of { tuple : 'expr; component : row_path; update : 'expr }
     (* sums *)
     | Inj of row_path * 'expr
     | Match of 'expr * 'expr row
