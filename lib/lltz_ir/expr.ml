f326615f17c48c2f2878c5e391fd9f26fdcd1f49
Alistair <ajo41@cam.ac.uk>
2024-06-18 19:21:51 +0100

    feat: remove recursion schemes by using ppxlib.traverse to generate traversals
    Note that the placement of type definitions within files is sub-optimal for this ppx extension

--- lib/lltz_ir/expr.ml
+++ lib/lltz_ir/expr.ml
@@ -1,200 +1,221 @@
-open Core
+open Import
 open Grace
-
-type type_ = Range.t Type.t [@@deriving sexp, equal, compare]
-type var = Var of string [@@deriving sexp, equal, compare]
-type mut_var = Mut_var of string [@@deriving sexp, equal, compare]
-
-type constant =
-  | Unit
-  | Bool of bool
-  | Nat of (Z.t[@sexp.opaque])
-  | Int of (Z.t[@sexp.opaque])
-  | Mutez of (Z.t[@sexp.opaque])
-  | String of string
-  | Key of string
-  | Key_hash of string
-  | Bytes of string
-  | Chain_id of string
-  | Address of string
-  | Timestamp of string
-  | Bls12_381_g1 of string
-  | Bls12_381_g2 of string
-  | Bls12_381_fr of string
-  | Signature of string
-[@@deriving sexp, equal, compare]
 
 type micheline = (Range.t, string) Tezos_micheline.Micheline.node
 
 module T = struct
-  type 'expr t =
+  type var = Var of string
+  and mut_var = Mut_var of string
+
+  and constant =
+    | Unit
+    | Bool of bool
+    | Nat of (Z.t[@sexp.opaque])
+    | Int of (Z.t[@sexp.opaque])
+    | Mutez of (Z.t[@sexp.opaque])
+    | String of string
+    | Key of string
+    | Key_hash of string
+    | Bytes of string
+    | Chain_id of string
+    | Address of string
+    | Timestamp of string
+    | Bls12_381_g1 of string
+    | Bls12_381_g2 of string
+    | Bls12_381_fr of string
+    | Signature of string
+
+  and t =
+    { desc : desc
+    ; range : Range.t
+    }
+
+  and desc =
     (* basic lambda calculus w/ primitives + constants *)
     | Var of var
     | Let_in of
         { let_var : var
-        ; rhs : 'expr
-        ; in_ : 'expr
+        ; rhs : t
+        ; in_ : t
         }
     | Lambda of
         { lam_var : var
-        ; return : type_
-        ; body : 'expr
+        ; return : Type.t
+        ; body : t
         }
     | Lambda_rec of
         { lam_var : var
         ; mu_var : var
-        ; return : type_
-        ; body : 'expr
+        ; return : Type.t
+        ; body : t
         }
     | App of
-        { abs : 'expr
-        ; arg : 'expr
+        { abs : t
+        ; arg : t
         }
     | Const of constant
-    | Prim of type_ Primitive.t * 'expr list
+    | Prim of Primitive.t * t list
     (* mutability *)
     | Let_mut_in of
         { let_var : mut_var
-        ; rhs : 'expr
-        ; in_ : 'expr
+        ; rhs : t
+        ; in_ : t
         }
     | Deref of mut_var
-    | Assign of mut_var * 'expr
+    | Assign of mut_var * t
     (* low-level control flow (conditional) *)
     | If_bool of
-        { condition : 'expr
-        ; if_true : 'expr
-        ; if_false : 'expr
+        { condition : t
+        ; if_true : t
+        ; if_false : t
         }
     | If_none of
-        { subject : 'expr
-        ; if_none : 'expr
-        ; if_some : var * 'expr
+        { subject : t
+        ; if_none : t
+        ; if_some : var * t
         }
     | If_cons of
-        { subject : 'expr
-        ; if_empty : 'expr
-        ; if_nonempty : var * var * 'expr
+        { subject : t
+        ; if_empty : t
+        ; if_nonempty : var * var * t
         }
     | If_left of
-        { subject : 'expr
-        ; if_left : var * 'expr
-        ; if_right : var * 'expr
+        { subject : t
+        ; if_left : var * t
+        ; if_right : var * t
         }
     (* low-level control flow (iterative) *)
     | While of
-        { invariant : 'expr
-        ; body : 'expr
+        { invariant : t
+        ; body : t
         }
     | While_left of
-        { invariant : 'expr
-        ; body : 'expr
+        { invariant : t
+        ; body : t
         }
     | For of
         { index : mut_var
-        ; init : 'expr
-        ; invariant : 'expr
-        ; variant : 'expr
-        ; body : 'expr
+        ; init : t
+        ; invariant : t
+        ; variant : t
+        ; body : t
         }
     | For_each of
         { indices : var list
-        ; collection : 'expr
-        ; body : 'expr
+        ; collection : t
+        ; body : t
         }
     (* high-level control flow (iterative) *)
     | Map of
-        { collection : 'expr
-        ; map : var list * 'expr
+        { collection : t
+        ; map : var list * t
         }
     | Fold_left of
-        { collection : 'expr
-        ; init : 'expr
-        ; fold : var * 'expr
+        { collection : t
+        ; init : t
+        ; fold : var * t
         }
     | Fold_right of
-        { collection : 'expr
-        ; init : 'expr
-        ; fold : var * 'expr
+        { collection : t
+        ; init : t
+        ; fold : var * t
         }
     (* tuples *)
     | Let_tuple_in of
         { components : var list
-        ; rhs : 'expr
-        ; in_ : 'expr
+        ; rhs : t
+        ; in_ : t
         }
-    | Tuple of 'expr Row.t
-    | Proj of 'expr * Row.Path.t
+    | Tuple of t Row.t
+    | Proj of t * Row.Path.t
     | Update of
-        { tuple : 'expr
+        { tuple : t
         ; component : Row.Path.t
-        ; update : 'expr
+        ; update : t
         }
     (* sums *)
-    | Inj of Row.Path.t * 'expr
-    | Match of 'expr * 'expr Row.t
+    | Inj of Row.Path.t * t
+    | Match of t * t Row.t
     (* tezos specific *)
     | Raw_michelson of (micheline[@sexp.opaque] [@equal.ignore] [@compare.ignore])
     | Create_contract of
-        { storage : type_
-        ; parameter : type_
-        ; code : 'expr
+        { storage : Type.t
+        ; parameter : Type.t
+        ; code : t
         }
-  [@@deriving sexp, equal, compare]
-
-  let map t ~f =
-    match t with
-    | Var v -> Var v
-    | Let_in { let_var; rhs; in_ } -> Let_in { let_var; rhs = f rhs; in_ = f in_ }
-    | Lambda { lam_var; return; body } -> Lambda { lam_var; return; body = f body }
-    | Lambda_rec { lam_var; mu_var; return; body } ->
-      Lambda_rec { lam_var; mu_var; return; body = f body }
-    | App { abs; arg } -> App { abs = f abs; arg = f arg }
-    | Const c -> Const c
-    | Prim (p, es) -> Prim (p, List.map es ~f)
-    | Let_mut_in { let_var; rhs; in_ } -> Let_mut_in { let_var; rhs = f rhs; in_ = f in_ }
-    | Deref v -> Deref v
-    | Assign (v, e) -> Assign (v, f e)
-    | If_bool { condition; if_true; if_false } ->
-      If_bool { condition = f condition; if_true = f if_true; if_false = f if_false }
-    | If_none { subject; if_none; if_some = x, if_some } ->
-      If_none { subject = f subject; if_none = f if_none; if_some = x, f if_some }
-    | If_cons { subject; if_empty; if_nonempty = x, xs, if_nonempty } ->
-      If_cons
-        { subject = f subject; if_empty = f if_empty; if_nonempty = x, xs, f if_nonempty }
-    | If_left { subject; if_left = x, if_left; if_right = y, if_right } ->
-      If_left { subject = f subject; if_left = x, f if_left; if_right = y, f if_right }
-    | While { invariant; body } -> While { invariant = f invariant; body = f body }
-    | While_left { invariant; body } ->
-      While_left { invariant = f invariant; body = f body }
-    | For { index; init; invariant; variant; body } ->
-      For
-        { index
-        ; init = f init
-        ; invariant = f invariant
-        ; variant = f variant
-        ; body = f body
-        }
-    | For_each { indices; collection; body } ->
-      For_each { indices; collection = f collection; body = f body }
-    | Map { collection; map = xs, map } ->
-      Map { collection = f collection; map = xs, f map }
-    | Fold_left { collection; init; fold = xs, fold } ->
-      Fold_left { collection = f collection; init = f init; fold = xs, f fold }
-    | Fold_right { collection; init; fold = xs, fold } ->
-      Fold_right { collection = f collection; init = f init; fold = xs, f fold }
-    | Let_tuple_in { components; rhs; in_ } ->
-      Let_tuple_in { components; rhs = f rhs; in_ = f in_ }
-    | Tuple row -> Tuple (Row.map row ~f)
-    | Proj (e, p) -> Proj (f e, p)
-    | Update { tuple; component; update } ->
-      Update { tuple = f tuple; component; update = f update }
-    | Inj (p, e) -> Inj (p, f e)
-    | Match (e, row) -> Match (f e, Row.map row ~f)
-    | Raw_michelson m -> Raw_michelson m
-    | Create_contract { storage; parameter; code } ->
-      Create_contract { storage; parameter; code = f code }
-  ;;
+  [@@deriving sexp, equal, compare, traverse]
 end
 
-include Recursion.Fixpoint (T)
+include T
+
+module Traverse = struct
+  class map =
+    let zero = Traverse_builtins.map_zero in
+    object
+      inherit Traverse_builtins.map
+      inherit Row.Traverse_builtins.map
+      inherit T.map
+      method z__t = zero
+      method type__t = zero
+      method row__path__t = zero
+      method range__t = zero
+      method primitive__t = zero
+      method micheline = zero
+    end
+
+  class iter =
+    let zero = Traverse_builtins.iter_zero in
+    object
+      inherit Traverse_builtins.iter
+      inherit Row.Traverse_builtins.iter
+      inherit T.iter
+      method z__t = zero
+      method type__t = zero
+      method row__path__t = zero
+      method range__t = zero
+      method primitive__t = zero
+      method micheline = zero
+    end
+
+  class ['acc] fold =
+    let zero = Traverse_builtins.fold_zero in
+    object
+      inherit ['acc] Traverse_builtins.fold
+      inherit ['acc] Row.Traverse_builtins.fold
+      inherit ['acc] T.fold
+      method z__t = zero
+      method type__t = zero
+      method row__path__t = zero
+      method range__t = zero
+      method primitive__t = zero
+      method micheline = zero
+    end
+
+  class ['acc] fold_map =
+    let zero = Traverse_builtins.fold_map_zero in
+    object
+      inherit ['acc] Traverse_builtins.fold_map
+      inherit ['acc] Row.Traverse_builtins.fold_map
+      inherit ['acc] T.fold_map
+      method z__t = zero
+      method type__t = zero
+      method row__path__t = zero
+      method range__t = zero
+      method primitive__t = zero
+      method micheline = zero
+    end
+
+  class ['ctx] map_with_context =
+    let zero = Traverse_builtins.map_with_context_zero in
+    object
+      inherit ['ctx] Traverse_builtins.map_with_context
+      inherit ['ctx] Row.Traverse_builtins.map_with_context
+      inherit ['ctx] T.map_with_context
+      method z__t = zero
+      method type__t = zero
+      method row__path__t = zero
+      method range__t = zero
+      method primitive__t = zero
+      method micheline = zero
+    end
+end
